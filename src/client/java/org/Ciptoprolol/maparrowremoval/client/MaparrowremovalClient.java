5a71500ed9513a1d76a039d1830e48a7bebbc120
cyproheptadine <kiarashshooshtary@outlook.com>
2026-07-21 22:06:15 -0700

    First commit with standard java code

---
+++ src/client/java/org/Ciptoprolol/maparrowremoval/client/MaparrowremovalClient.java
@@ -0,0 +1,10 @@
+package org.Ciptoprolol.maparrowremoval.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaparrowremovalClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/Ciptoprolol/maparrowremoval/Maparrowremoval.java
@@ -0,0 +1,10 @@
+package org.Ciptoprolol.maparrowremoval;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Maparrowremoval implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
